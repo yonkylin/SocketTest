599c5a4d3aaeda209f0fccbc7265e3c9e5a52730
yonkylin <yonkylin@qq.com>
2018-04-19 17:36:25 +0800

    first commit

---
+++ app/src/main/java/yonky/sockettest/MainActivity.java
@@ -0,0 +1,133 @@
+package yonky.sockettest;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public static String TAG="SOCKET CLIENT";
+    private static final int MESSAGE_RECEIVE_NEW_MSG=1;
+    private static final int MESSAGE_SOCKET_CONNECTED = 2;
+
+    private Button mSendButton;
+    private TextView mMessageTextView;
+
+    private Socket mClientSocket;
+    private PrintWriter mPrintWriter;
+    @SuppressLint("handlerLeak")
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case MESSAGE_RECEIVE_NEW_MSG:{
+                    mMessageTextView.setText(mMessageTextView.getText()+(String)msg.obj);
+                    break;
+                }
+                case MESSAGE_SOCKET_CONNECTED:{
+                    mSendButton.setEnabled(true);
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSendButton = (Button) findViewById(R.id.send);
+        mMessageTextView = (TextView) findViewById(R.id.msg_container);
+        mSendButton.setOnClickListener(this);
+        Intent service = new Intent(this,TCPServerService.class);
+        startService(service);
+        new Thread(new TcpClient()).start();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view ==mSendButton){
+            final String msg = mMessageEditText.getText().toString();
+        }
+    }
+
+    private class TcpClient implements Runnable{
+        @Override
+        public void run() {
+            Socket socket=null;
+            while (socket==null){
+                try{
+                    socket = new Socket("localhost",8688);
+                    mClientSocket = socket;
+                    mPrintWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
+                    mHandler.sendEmptyMessage(MESSAGE_SOCKET_CONNECTED);
+                    Log.i(TAG,"connect server success");
+
+
+                }catch (IOException e){
+                    SystemClock.sleep(1000);
+                    Log.e(TAG,"connect tcp server failed,retry...");
+                }
+            }
+            try{
+                //接收服务器端的消息
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while(!MainActivity.this.isFinishing()){
+                    String msg = br.readLine();
+                    Log.i(TAG,"receive:"+msg);
+                    if(msg !=null){
+                        String time = formatDataTime(System.currentTimeMillis());
+                        final String showedMsg = "server"+time +":"+msg+"\n";
+                        mHandler.obtainMessage(MESSAGE_RECEIVE_NEW_MSG,showedMsg).sendToTarget();
+                    }
+                }
+                Log.i(TAG,"quit...");
+
+                mPrintWriter.close();
+                br.close();
+                socket.close();
+            }catch (IOException e){
+                e.printStackTrace();
+
+
+
+            }
+        }
+    }
+    private String formatDataTime(long time){
+        return new SimpleDateFormat("(HH:mm:ss)").format(new Date(time));
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(mClientSocket !=null){
+            try{
+                mClientSocket.shutdownInput();
+                mClientSocket.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/yonky/sockettest/TCPServerService.java
@@ -0,0 +1,106 @@
+package yonky.sockettest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+/**
+ * Created by Administrator on 2018/4/19.
+ */
+
+public class TCPServerService extends Service {
+    private static String TAG="TCPServer";
+    private boolean mIsServiceDestroyed = false;
+    private String[] mDefinedMessages=new String[]{
+            "你好啊，哈哈",
+            "请问你叫什么名字呀？",
+            "今天天气真不错",
+            "你知道吗？我可是可以和多个人同事聊天的哦"
+    };
+
+    @Override
+    public void onCreate() {
+        new Thread(new TcpServer()).start();
+        super.onCreate();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        mIsServiceDestroyed = true;
+        super.onDestroy();
+    }
+
+    private class TcpServer implements Runnable{
+        @Override
+        public void run() {
+            ServerSocket serverSocket = null;
+            try{
+                serverSocket = new ServerSocket(8688);
+            }catch (IOException e){
+                Log.e(TAG,"establish tcp server failed,port 8688");
+                e.printStackTrace();
+                return;
+            }
+
+            while (!mIsServiceDestroyed){
+                try{
+                    final Socket client = serverSocket.accept();
+                    Log.i(TAG,"accept");
+                    new Thread(){
+                        @Override
+                        public void run(){
+                            try{
+                                responseClient(client);
+                            }catch (IOException e){
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private  void responseClient(Socket client) throws IOException{
+        //用于接收客户端信息
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        //用于向客户端发送消息
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())),true);
+        out.print("欢迎来到聊天室");
+        while (!mIsServiceDestroyed){
+            String str= in.readLine();
+            Log.i(TAG,str);
+            if(str==null){
+                break;
+            }
+            int i = new Random().nextInt(mDefinedMessages.length);
+            String msg = mDefinedMessages[i];
+            out.println(msg);
+            Log.i(TAG,"send:"+msg);
+        }
+        Log.i(TAG,"client quit.");
+        out.close();
+        in.close();
+        client.close();
+    }
+}
